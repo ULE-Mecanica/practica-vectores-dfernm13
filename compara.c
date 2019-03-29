d7d63309300096d52b13fc1d9fc32120b93a4c6a
dfernm13 <47888069+dfernm13@users.noreply.github.com>
2019-03-29 10:22:50 +0100

    Add files via upload

---
+++ compara.c
@@ -0,0 +1,29 @@
+// EJERCICIO2: Un programa carge una funcion que me pida dos vectores de 5 elementos y otra funcion me dijera si estos son iguales elemento a elemento, A=1,2,3,4,5 y B=1,2,3,4,5;son iguales, A=1,2,3,4,5 y B=1,3,2,4,5; Son distintos.
+#include <stdio.h>
+#include<stdlib.h>
+#define TAM 5
+void cargar(int v[5]);
+int compara (int v1[5], int v2[5]);
+int main() {
+int v1={1,2,3,4,5};
+printf("Dame los 5 valores: \n");
+scanf ("%d",&v[5]);
+cargar (v1);
+cargar (v2);
+}
+void cargar (int v[5]){
+int i;
+int iguales=1;
+for(i=0;i<TAM;i++){
+scanf("%d",&v[i]);
+if (v1[1]!=v2[1]){
+}
+}
+int compara(int v1[5],int v2[5]){
+if (compara(v1[5],v2[5])){
+printf(" Los vectores son iguales \n");
+}else{
+if (printf("Los vectores no son iguales\n"));
+return compara;
+}
+}
